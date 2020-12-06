ee8d939deaac3a5440e05df61f3fea242da7f1d5
Eduardo Medina <eduardo.medina@ext.leroymerlin.es>
2020-12-06 02:20:30 +0100

    fix width heigth ratio

--- app/src/main/java/com/dabyz/camera1/MainActivity.kt
+++ app/src/main/java/com/dabyz/camera1/MainActivity.kt
@@ -2,53 +2,78 @@ package com.dabyz.camera1
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.hardware.Camera
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.View
+import android.view.ViewGroup.MarginLayoutParams
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.system.exitProcess
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState); setContentView(R.layout.activity_main)
-        requestPermissions()
     }
 
-    fun init() {
-        var camera = Camera.open()
+    override fun onResume() {
+        super.onResume()
+        fun hasPermission() = listOf(Manifest.permission.CAMERA)
+            .filterNot { ActivityCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED }
+            .let { needs ->
+                if (needs.isNotEmpty()) ActivityCompat.requestPermissions(this, needs.toTypedArray(), 0)
+                needs.isEmpty()
+            }
+        if (hasPermission()) init()
+    }
+
+    private fun openCamera() = Camera.open().also { camera ->
         var params = camera.parameters
-
-        var size = params.getSupportedPictureSizes().filter { it.width > 300 && it.height > 300 }.last()
+        fun ratio(size: Camera.Size) = size.width.toDouble() / size.height.toDouble()
+        var size = params.getSupportedPictureSizes().filter { s -> s.height > 300 }
+            .filter { s -> params.getPreferredPreviewSizeForVideo().let { p -> abs(ratio(p) - ratio(s)) <= 0.01 } }
+            .minBy { it.height }!!
         params.setPictureSize(size.width, size.height)
-
-        var or = params.get("orientation")
-        //var o2 = camera.getDisplayOrientation()
-        params.set("orientation", "portrait")
-        params.setRotation(90)
-        camera.parameters = params
         camera.setDisplayOrientation(90)
+        camera.parameters = params
 
-        camera_preview.addView(CamPreView(this, size.height,size.width, camera))
+        camera_preview.addView(SurfaceView(this).also {
+            it.holder.addCallback(object : SurfaceHolder.Callback {
+                override fun surfaceCreated(holder: SurfaceHolder) {
+                    camera.setPreviewDisplay(holder); camera.startPreview()
+                }
 
+                override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
+                override fun surfaceDestroyed(holder: SurfaceHolder) {
+                    camera.stopPreview(); camera.release()
+                }
+            })
+        })
+        //TODO "ADD AUTO FOCUS"
+    }
+
+    private fun init() = openCamera().let { camera ->
+        fun View.changeTop(t: Int) = { (layoutParams as MarginLayoutParams).topMargin = t; requestLayout() }()
+        fun getScreenWidth() = DisplayMetrics().let { d -> windowManager.defaultDisplay.getMetrics(d); d.widthPixels }
+        front_ground.changeTop(getScreenWidth())
         button.setOnClickListener {
             camera.takePicture(null, null) { data, _ ->
                 val bitmap = BitmapFactory.decodeByteArray(data, 0, data.size)
-                captured_image.setImageBitmap(bitmap)
+                fun rotate(bmp: Bitmap) = Matrix().let { it.postRotate(90F); Bitmap.createBitmap(bmp, 0, 0, bmp.width, bmp.height, it, true) }
+                fun cut(bmp: Bitmap) = minOf(bmp.width, bmp.height).let { Bitmap.createBitmap(bmp, 0, 0, it, it) }
+                captured_image.setImageBitmap(rotate(cut(bitmap))) //TODO "Scale down to 300 px
+                camera_preview.visibility = View.GONE; button.visibility = View.GONE; captured_image.visibility = View.VISIBLE
             }
         }
-    }
-
-    private fun requestPermissions() {
-        var permissions = listOf(Manifest.permission.CAMERA).filterNot { ActivityCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED }
-            .toTypedArray()
-        if (permissions.isNotEmpty()) ActivityCompat.requestPermissions(this, permissions, 0) else init()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 0 && grantResults.filterNot { it == PackageManager.PERMISSION_GRANTED }.isNotEmpty()) exitProcess(0) else init()
+        captured_image.setOnClickListener {
+            camera.startPreview()
+            camera_preview.visibility = View.VISIBLE; button.visibility = View.VISIBLE; captured_image.visibility = View.GONE
+        }
     }
 }
