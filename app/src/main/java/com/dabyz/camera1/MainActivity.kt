1ddbdcdcc0f2f42fc504310780f31ee6c35e1eb6
Eduardo Medina <eduardo.medina@ext.leroymerlin.es>
2021-01-03 23:07:00 +0100

    auto focus added

--- app/src/main/java/com/dabyz/camera1/MainActivity.kt
+++ app/src/main/java/com/dabyz/camera1/MainActivity.kt
@@ -8,6 +8,7 @@ import android.graphics.Matrix
 import android.hardware.Camera
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.view.View
@@ -15,6 +16,7 @@ import android.view.ViewGroup.MarginLayoutParams
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.ByteArrayOutputStream
 import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
@@ -34,15 +36,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun openCamera() = Camera.open().also { camera ->
-        var params = camera.parameters
-        fun ratio(size: Camera.Size) = size.width.toDouble() / size.height.toDouble()
-        var size = params.getSupportedPictureSizes().filter { s -> s.height > 300 }
-            .filter { s -> params.getPreferredPreviewSizeForVideo().let { p -> abs(ratio(p) - ratio(s)) <= 0.01 } }
-            .minBy { it.height }!!
-        params.setPictureSize(size.width, size.height)
+        camera.parameters.let { params ->
+            fun ratio(size: Camera.Size) = size.width.toDouble() / size.height.toDouble()
+            params.getSupportedPictureSizes().filter { s -> s.height < 2000 }
+                .filter { s -> params.getPreferredPreviewSizeForVideo().let { p -> abs(ratio(p) - ratio(s)) <= 0.01 } }
+                .maxBy { it.height }!!
+                .let { size -> params.setPictureSize(size.width, size.height) }
+            params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)
+            camera.parameters = params
+        }
         camera.setDisplayOrientation(90)
-        camera.parameters = params
-
         camera_preview.addView(SurfaceView(this).also {
             it.holder.addCallback(object : SurfaceHolder.Callback {
                 override fun surfaceCreated(holder: SurfaceHolder) {
@@ -55,7 +58,6 @@ class MainActivity : AppCompatActivity() {
                 }
             })
         })
-        //TODO "ADD AUTO FOCUS"
     }
 
     private fun init() = openCamera().let { camera ->
@@ -63,11 +65,24 @@ class MainActivity : AppCompatActivity() {
         fun getScreenWidth() = DisplayMetrics().let { d -> windowManager.defaultDisplay.getMetrics(d); d.widthPixels }
         front_ground.changeTop(getScreenWidth())
         button.setOnClickListener {
-            camera.takePicture(null, null) { data, _ ->
-                val bitmap = BitmapFactory.decodeByteArray(data, 0, data.size)
-                fun rotate(bmp: Bitmap) = Matrix().let { it.postRotate(90F); Bitmap.createBitmap(bmp, 0, 0, bmp.width, bmp.height, it, true) }
+            camera.takePicture(null, null) { photo, _ ->
+                Log.e(null, "photo.size:${photo.size}")
+                fun scale(photo: ByteArray, width: Int): Bitmap = BitmapFactory.Options().let { op ->
+                    with(op) { inScaled = true; inDensity = width; inTargetDensity = 77 }
+                    BitmapFactory.decodeByteArray(photo, 0, photo.size, op)
+                }
                 fun cut(bmp: Bitmap) = minOf(bmp.width, bmp.height).let { Bitmap.createBitmap(bmp, 0, 0, it, it) }
-                captured_image.setImageBitmap(rotate(cut(bitmap))) //TODO "Scale down to 300 px
+                fun rotate(bmp: Bitmap) = Matrix().let { it.postRotate(90F); Bitmap.createBitmap(bmp, 0, 0, bmp.width, bmp.height, it, true) }
+                fun scale(bmp: Bitmap): Bitmap = BitmapFactory.Options().let { op ->
+                    with(op) { inScaled = true; inDensity = bmp.width; inTargetDensity = 500 }
+                    bmp2Bytes(bmp).let { BitmapFactory.decodeByteArray(it, 0, it.size, op) }
+                }
+                val bitmap0 = BitmapFactory.decodeByteArray(photo, 0, photo.size)
+                bmpLog(bitmap0)
+                val bitmap = rotate(scale(cut(BitmapFactory.decodeByteArray(photo, 0, photo.size))))
+                bmpLog(bitmap)
+
+                captured_image.setImageBitmap(bitmap)
                 camera_preview.visibility = View.GONE; button.visibility = View.GONE; captured_image.visibility = View.VISIBLE
             }
         }
@@ -75,5 +90,13 @@ class MainActivity : AppCompatActivity() {
             camera.startPreview()
             camera_preview.visibility = View.VISIBLE; button.visibility = View.VISIBLE; captured_image.visibility = View.GONE
         }
+    }
+
+    fun bmp2Bytes(bmp: Bitmap): ByteArray = ByteArrayOutputStream()
+        .let { stream -> bmp.compress(Bitmap.CompressFormat.WEBP, 80, stream).let { stream.toByteArray() } }
+
+    fun bmpLog(b: Bitmap) {
+        Log.e(null, "d:${b.rowBytes * b.height}-w:${b.width}-h:${b.height}-d:${b.density}")
+        Log.e(null, "photo.size:${bmp2Bytes(b).size}")
     }
 }
